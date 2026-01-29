c7d85336869cb0487525f77cf459aae713d2daa2
DikshitG <gyawalidikshit15@gmail.com>
2026-01-29 18:54:07 +0545

    Update quadratic equation solver in ex13.c
    Refactored quadratic equation solver to handle both real and imaginary roots.

--- chapter 2 exercise/ex13.c
+++ chapter 2 exercise/ex13.c
@@ -1,12 +1,31 @@
-#include<stdio.h>
-#include<math.h> //Roots of quqrdatic function
-int main(){
-    double a,b,c,D,x1,x2;
-    printf("Enter a,b,c:");
-    scanf("%lf %lf %lf",&a,&b,&c);
-    D =  b*b -4*a*c;
-    x1 = (-b+sqrt(D));
-    x2 =(-b-sqrt(D));
-    printf("x1 = %lf\n x2 =%lf",x1,x2);
-    return 0;
+#include<stdio.h>//roots of quardatic equation
+#include<math.h>
+#include<conio.h>
+void main(){
+    float a,b,c,d,r1,r2,real,img;
+    printf("Enter a value of a,b,c:");
+    scanf("%f %f %f",&a,&b,&c);
+    if(a!=0){
+        d=b*b-4*a*c;
+        if(d>=0){
+            d=sqrt(d);
+            r1=-b/(2*a)+d/(2*a);
+            r2=-b/(2*a)-d/(2*a);
+            printf("\nr1=%.1f\n",r1);
+            printf("r2=%.1f",r2);
+        }
+        else{
+            //Imaginary roots
+            d=sqrt(fabs(d));
+            real = -b/(2*a);
+            img=d/(2*a);
+            printf("\nr1=%1.f+%.1fi\n",real,img);
+            printf("\nr2=%1.f-%.1fi\n",real,img);
+
+        }
+
+    }else{
+        ("\nInvalid output");
+    }
+    getch();
 }
